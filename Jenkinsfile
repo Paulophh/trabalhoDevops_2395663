a957bfb7dfc81a93dc013b3421b9001677d734e6
Paulo <pauloapts1@hotmail.com>
2024-12-05 21:07:05 -0300

    Primeiro Commit do Projeto, 90 % completo

---
+++ Jenkinsfile
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    environment {
+        REPOSITORY_URL = 'https://github.com/Paulophh/trabalho-devops.git'
+        BRANCH_NAME = 'main'
+    }
+
+    stages {
+        stage('Baixar código do Git') {
+            steps {
+                // Clonar o repositório do Git
+                git branch: "${BRANCH_NAME}", url: "${REPOSITORY_URL}"
+            }
+        }
+
+        stage('Build e Deploy') {
+            steps {
+                script {
+                    // Construir as imagens Docker para cada serviço
+                    sh '''
+                        docker-compose build
+                    '''
+
+                    // Subir os containers do Docker com Docker Compose
+                    sh '''
+                        docker-compose up -d
+                    '''
+                }
+            }
+        }
+
+        stage('Rodar Testes') {
+            steps {
+                script {
+                    // Rodar os testes dentro do contêiner de testes com Docker Compose
+                    sh '''
+                        docker exec pipeline_trabalhodevops_flask_1 python -m unittest test_app.py
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline executada com sucesso!'
+        }
+        failure {
+            echo 'A pipeline falhou.'
+        }
+    }
+}
---
+++ flask/app.py
@@ -0,0 +1,39 @@
+# Código principal do Flask (app.py)
+import time
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_appbuilder import AppBuilder, SQLA
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import ModelView
+from sqlalchemy.exc import OperationalError
+from prometheus_flask_exporter import PrometheusMetrics
+import logging
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+# Configuração da chave secreta para entrada em sessoes
+app.config['SECRET_KEY'] = 'minha_chave_secreta_super_secreta'  #Substitua pela sua chave segura
+
+# Configuração do db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root_password@mariadb/school_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar o dbb e o appBuild
+db = SQLAlchemy(app)
+appbuilder = AppBuilder(app, db.session)
+
+# Configuração do log
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Modelo de Aluno - Definição da tabela aluno no db
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    sobrenome = db.Column(db.String(50), nullable=False)
+    turma = db.Column(db.String(50), nullable=False)
+    disciplinas = db.Column(db.String(200), nullable=False)
+    ra = db.Column(db.String(200), nullable=False)
+
+
